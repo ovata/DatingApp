9a9b005337db23e2bf50391c8b86d4cb8d8eec19
ovata <ovataworld@gmail.com>
2021-11-18 12:20:02 +0100

    adding error handling api and pages

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,3 +1,5 @@
+import { ServerErrorComponent } from './errors/server-error/server-error.component';
+import { TestErrorsComponent } from './errors/test-errors/test-errors.component';
 import { ListsComponent } from './lists/lists.component';
 import { MemberDetailComponent } from './members/member-detail/member-detail.component';
 import { MemberListComponent } from './members/member-list/member-list.component';
@@ -6,21 +8,25 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { MessagesComponent } from './messages/messages.component';
 import { AuthGuard } from './_guard/auth.guard';
+import { NotFoundComponent } from './errors/not-found/not-found.component';
 
 const routes: Routes = [
-  {path: '', component: HomeComponent},
+  { path: '', component: HomeComponent },
   {
-      path: ' ',
-      runGuardsAndResolvers: 'always',
-      canActivate: [AuthGuard],
-      children: [
-        {path: 'members', component: MemberListComponent},
-        {path: 'members/:id', component: MemberDetailComponent, canActivate: [AuthGuard]},
-        {path: 'lists', component: ListsComponent},
-        {path: 'messages', component: MessagesComponent},
-      ]
-    },
-  {path: '**', component: HomeComponent, pathMatch: 'full'},
+    path: ' ',
+    runGuardsAndResolvers: 'always',
+    canActivate: [AuthGuard],
+    children: [
+      { path: 'members', component: MemberListComponent, canActivate: [AuthGuard] },
+      { path: 'members/:id', component: MemberDetailComponent },
+      { path: 'lists', component: ListsComponent },
+      { path: 'messages', component: MessagesComponent },
+    ]
+  },
+  { path: 'errors', component: TestErrorsComponent },
+  { path: 'not-found', component: NotFoundComponent },
+  { path: 'server-error', component: ServerErrorComponent },
+  { path: '**', component: NotFoundComponent, pathMatch: 'full' },
 ];
 
 @NgModule({
